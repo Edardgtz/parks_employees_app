7d9207c5ee6d178316a26a22fc967da075ab0637
Eduardo Gutierrez <eduardo.gutierrez643@gmail.com>
2020-11-22 11:33:40 -0600

    add create action to employee model

--- app/controllers/api/employees_controller.rb
+++ app/controllers/api/employees_controller.rb
@@ -9,4 +9,19 @@ class Api::EmployeesController < ApplicationController
     @employee = Employee.find_by(id: params[:id])
     render 'show.json.jb'
   end
+
+  def create
+    @employee = Employee.create(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      phone_number: params[:phone_number],
+      email: params[:email],
+      department_id: params[:department_id],
+      hire_date: params[:hire_date],
+      active_status: params[:active_status],
+      title: params[:title]
+    )
+    render 'show.json.jb'
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   namespace :api do
     get "/employees" => "employees#index"
     get "/employees/:id" => "employees#show"
-     
+    post "/employees" => "employees#create" 
   end
 end
